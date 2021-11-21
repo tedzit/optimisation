f833a1c45d98104c382e104d9f5580006bd72372
tedzit <morell.ted.902@gmail.com>
2021-11-21 13:49:08 +0100

    plot graph

--- cannonical_basis.py
+++ cannonical_basis.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Interpolation algorithm of a set of data  by a polynome in canonical basis
+# Interpolation algorithm of a set of data  by a polynomal in canonical basis
 
 # Input data in 2D
 def getPolynomial(D):
@@ -19,10 +19,31 @@ def getPolynomial(D):
     print(B)
     X = np.linalg.inv((A.transpose()).dot(A)).dot(A.transpose().dot(B))
     print(X)
+    return X
+
+def drawPolynomial(xs, coeffs):
+    n=len(coeffs)
+    ys = np.zeros(len(xs))
+    for i in range(n):
+        ys += coeffs[i]*xs**i
+    return ys
 
 
 # main function
 if __name__ == "__main__":
     # Input data xi, yi
     X = np.array([[-1, 2],[1, 0],[2, 1]])
-    getPolynomial(X)    
+    A = getPolynomial(X)    
+
+    # Plot data
+    xmin = -5
+    xmax = 5
+    # plot points to fit
+    plt.scatter(X[:,0], X[:,1], color="red")
+    # plot polynomial curb
+    xs = np.linspace(xmin,xmax)
+    ys = drawPolynomial(xs,A)
+    plt.plot(xs,ys, color="blue")
+    plt.show()
+
+
